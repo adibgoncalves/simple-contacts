c6a1347fffb933f4e496f22c2c8a8d151b6010c4
Adib Gonçalves Fernandes de souza <adib@facex.local>
2021-05-31 18:49:56 -0300

    Criado a tela de criar contato, de mostrar, botão de voltar e ajustes de CSS

--- config/process.php
+++ config/process.php
@@ -5,9 +5,25 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id;
+    if(!empty($_GET)) {
+        $id = $_GET["id"];
+    }
 
-    $query = "SELECT * FROM contacts";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $contacts = $stmt->fetchAll();
+    // Retorna o dado de um contato
+    if(!empty($id)) {
+        $query = "SELECT * FROM contacts WHERE id = :id";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $contact = $stmt->fetch();
+
+    }else {
+        // Retorna todos os contatos
+        $contacts = [];
+        $query = "SELECT * FROM contacts";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $contacts = $stmt->fetchAll();
+    }
+
